a3afea1c2c0c51e0338cf852201d3f85744878bd
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-03-23 23:54:43 +0300

    adding breadthfirst treverse

--- src/Graph.java
+++ src/Graph.java
@@ -96,6 +96,35 @@ public class Graph {
                 traverseDepthFirst(node, visited);
     }
 
+    public void traverseBreadthFirst(String root){
+
+        var node = nodes.get(root);
+        if(node == null)
+            return;
+
+        Set<Node> visited = new HashSet<>();
+
+        Queue<Node> queue = new ArrayDeque<>();
+
+        queue.add(node);
+
+        while(!queue.isEmpty()){
+            var current = queue.remove();
+
+            if(visited.contains(current))
+                continue;
+
+            System.out.println(current);
+            visited.add(current);
+
+            for(var neighbour : adjacencyList.get(current))
+                if(!visited.contains(neighbour))
+                    queue.add(neighbour);
+        }
+
+
+    }
+
 
 
 }
